a8814355d8535a0ac7e473b7c1fae744eb699a77
DrBibop <olipoulihot@hotmail.com>
2023-06-26 21:46:05 -0400

    First push

---
+++ Archipelago_Inscryption/ArchipelagoInscryption.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace Archipelago_Inscryption
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public class ArchipelagoInscryption : BaseUnityPlugin
+    {
+        internal const string PluginGuid = "ballininc.inscryption.archipelagomod";
+        internal const string PluginName = "ArchipelagoMod";
+        internal const string PluginVersion = "0.0.1";
+
+        internal static ManualLogSource Log;
+
+        private void Awake()
+        {
+            Harmony harmony = new Harmony(PluginGuid);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
